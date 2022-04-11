7caf8514a78d44874ce021225fd25c949ce40402
AHTepsilon <alehertoro725@gmail.com>
2022-04-11 17:25:27 -0500

    managed to add other screens TODO: FIX STUDENTS QUESTION TIMING

--- src/sketch.js
+++ src/sketch.js
@@ -2,6 +2,10 @@ let student1, student2, student3, student4, student5, student6;
 let timeout_phase1, timeout2_phase1, timeout3_phase1, timeout4_phase1, timeout5_phase1;
 let timeout_phase2, timeout2_phase2, timeout3_phase2, timeout4_phase2, timeout5_phase2;
 
+let screenPic1, screenPic2, screenPic3, screenPic4, screenPic5, screenPic6;
+
+let screen;
+
 let studentsArr;
 
 let phaseNum;
@@ -13,6 +17,14 @@ let timerValue, timerValueMinutes;
 let gameCanvas;
 
 function preload() {
+
+  screenPic1 = loadImage("./img/screen1.png");
+  screenPic2 = loadImage("./img/screen2.png");
+  screenPic3 = loadImage("./img/screen3.png");
+  screenPic4 = loadImage("./img/screen4.png");
+  screenPic5 = loadImage("./img/screen5.png");
+  screenPic6 = loadImage("./img/screen6.png");
+
   bg = loadImage("./img/bg.png");
   desk = loadImage("./img/desk.png");
 }
@@ -23,13 +35,14 @@ function setup() {
   gameCanvas.parent("gameContainer");
 
   phaseNum = 0;
+  screen = 0;
 
-  student1 = new Student1(258, 215);
+  student1 = new Student1(258, 174);
   student2 = new Student2(202, 351);
   student3 = new Student3(144, 502);
   student4 = new Student4(975, 215);
   student5 = new Student5(1052, 351);
-  student6 = new Student6(1132, 502);
+  student6 = new Student6(1132, 531);
 
   studentsArr = [student1, student2, student3, student4, student5, student6];
 
@@ -38,59 +51,104 @@ function setup() {
   timerValue = 0;
   timerValueMinutes = 3;
 
-  setInterval(timeIt, 1000);
+    setInterval(timeIt, 1000);
 
-  timeout = setInterval((ev) => {
-    student1.calledTeacher = true;
-  }, Math.floor(Math.random() * (10000 - 4000) + 4000));
+    //if(screen === 6){
+    timeout = setInterval((ev) => {
+      student1.calledTeacher = true;
+    }, Math.floor(Math.random() * (10000 - 4000) + 4000) + frameCount);
+  
+    timeout2 = setInterval((ev) => {
+      student2.calledTeacher = true;
+    }, Math.floor(Math.random() * (18000 - 7000) + 7000)  + frameCount);
+  
+    timeout3 = setInterval((ev) => {
+      student3.calledTeacher = true;
+    }, Math.floor(Math.random() * (20000 - 5000) + 5000)  + frameCount);
+  
+    timeout4 = setInterval((ev) => {
+      student4.calledTeacher = true;
+    }, Math.floor(Math.random() * (24000 - 8000) + 8000)  + frameCount);
+  
+    timeout5 = setInterval((ev) => {
+      student5.calledTeacher = true;
+    }, Math.floor(Math.random() * (32000 - 24000) + 24000)  + frameCount);
+  
+    timeout6 = setInterval((ev) => {
+      student6.calledTeacher = true;
+    }, Math.floor(Math.random() * (28000 - 16000) + 16000) + frameCount);
 
-  timeout2 = setInterval((ev) => {
-    student2.calledTeacher = true;
-  }, Math.floor(Math.random() * (18000 - 7000) + 7000));
-
-  timeout3 = setInterval((ev) => {
-    student3.calledTeacher = true;
-  }, Math.floor(Math.random() * (20000 - 5000) + 5000));
-
-  timeout4 = setInterval((ev) => {
-    student4.calledTeacher = true;
-  }, Math.floor(Math.random() * (24000 - 8000) + 8000));
-
-  timeout5 = setInterval((ev) => {
-    student5.calledTeacher = true;
-  }, Math.floor(Math.random() * (32000 - 24000) + 24000));
-
-  timeout6 = setInterval((ev) => {
-    student6.calledTeacher = true;
-  }, Math.floor(Math.random() * (28000 - 16000) + 16000));
+  //}
 }
 
 function draw() {
-  console.log(mouseX + ", " + mouseY);
 
   imageMode(CORNER);
   background(255);
-  image(bg, 0, 0);
+  //console.log(mouseX + ", " + mouseY);
+  console.log(frameCount);
 
+  if(screen === 0){
+    
+    image(screenPic1, 0, 0);
+
+  }
+
+  else if(screen === 1){
+    
+    image(screenPic2, 0, 0);
+
+  }
+
+  else if(screen === 2){
+
+    image(screenPic3, 0, 0);
+
+  }
+
+  else if(screen === 3){
+
+    image(screenPic4, 0, 0);
+
+  }
+
+  else if(screen === 4){
+
+    image(screenPic5, 0, 0);
+
+  }
+
+  else if(screen === 5){
+
+    image(screenPic6, 0, 0);
+
+  }
+
+  else if(screen === 6){
+
+    image(bg, 0, 0);
   
-  studentsArr.forEach((stud) => {
+    
+    studentsArr.forEach((stud) => {
+  
+      stud.paint();
+      stud.callTeacher();
+  
+      if(stud.questionActive){
+        stud.showProblem();
+      }
+  
+    });
+  
+    imageMode(CORNER);
+    image(desk, 5, 531);
+  
+    timer();
+    endGame();
+  
+    changePhase();
 
-    stud.paint();
-    stud.callTeacher();
-
-    if(stud.questionActive){
-      stud.showProblem();
-    }
-
-  });
-
-  imageMode(CORNER);
-  image(desk, 5, 531);
-
-  timer();
-  endGame();
-
-  changePhase();
+  }
 }
 
 function timer() {
@@ -138,28 +196,94 @@ function endGame() {
 function mousePressed() {
   console.log("Click");
 
-  studentsArr.forEach(stud => {
+  if(screen === 0){
 
-    if(dist(mouseX, mouseY, stud.posX - 150, stud.posY - 75) < 100){
+    if(mouseX > 534 && mouseY > 523 && mouseX < 745 && mouseY < 583){
 
-      console.log(stud + " clicked");
-
-      stud.calledTeacher = false;
-      stud.questionActive = true;
+    screen = 1;
 
     }
 
-    if (
-      mouseX > 459 &&
-      mouseY > 236 &&
-      mouseX < 743 &&
-      mouseY < 495 &&
-      stud.questionActive
-    ) {
-      stud.questionActive = false;
+  }
+
+  else if(screen === 1){
+
+    if(mouseX > 534 && mouseY > 523 && mouseX < 745 && mouseY < 583){
+
+    screen = 2;
+
+    }
+  
+  }
+
+  else if(screen === 2){
+
+    if(mouseX > 1139 && mouseY > 574 && mouseX < 1243 && mouseY < 605){
+
+    screen = 3;
+
     }
 
-  });
+  }
+
+  else if(screen === 3){
+
+    if(mouseX > 1139 && mouseY > 574 && mouseX < 1243 && mouseY < 605){
+
+    screen = 4;
+
+    }
+
+  }
+
+  else if(screen === 4){
+
+    if(mouseX > 1139 && mouseY > 574 && mouseX < 1243 && mouseY < 605){
+
+    screen = 5;
+
+    }
+
+  }
+
+  else if(screen === 5){
+
+    if(mouseX > 1139 && mouseY > 574 && mouseX < 1243 && mouseY < 605){
+
+    screen = 6;
+
+    }
+
+  }
+
+  else if(screen === 6){
+
+    studentsArr.forEach(stud => {
+
+      if(dist(mouseX, mouseY, stud.posX - 150, stud.posY - 75) < 100){
+  
+        console.log(stud + " clicked");
+  
+        stud.calledTeacher = false;
+        stud.questionActive = true;
+  
+      }
+  
+      if (
+        mouseX > 459 &&
+        mouseY > 236 &&
+        mouseX < 743 &&
+        mouseY < 495 &&
+        stud.questionActive
+      ) {
+        stud.questionActive = false;
+      }
+  
+    });
+
+  }
+
+
 }
 
 function keyPressed() {
